040d0967c9b8d36466dcf6a47f405fdf079551db
Toesh <p.t.vandenbos@gmail.com>
2019-07-23 13:05:38 +0200

    #25 Add recipe interface definition

--- frontend/src/app/interfaces/recipe.interface.ts
+++ frontend/src/app/interfaces/recipe.interface.ts
@@ -1,4 +1,39 @@
 export interface IRecipe {
 	_id: string;
+	alternatives: string;
+	cooking_time: number;
+	description: string;
+	difficulty: Difficulty;
+	image: string;
+	ingredients: string[];
+	link: string;
+	nutrition: INutrition; // 1 Serving
+	preparation_time: number;
+	ranking: number;
 	recipe_name: string;
+	steps: string[];
+	tags: string[];
+}
+
+interface INutrition {
+	kcal: number;
+	fat: IFat;
+	carbohydrates: number; // grams
+	sugar: number; // grams
+	fibre: number; // grams
+	protein: number; // grams
+	salt: number; // grams
+}
+
+interface IFat {
+	unsaturated: number; // grams
+	monounsaturated: number; // grams
+	polyunsaturated: number; // grams
+	trans: number; // grams
+}
+
+enum Difficulty {
+	easy,
+	medium,
+	hard
 }
